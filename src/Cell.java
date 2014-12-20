97e668f4ad5dab16a9972be85ac1c0c3b040d0e4
Kate Ross <kateross1227@gmail.com>
2014-12-19 20:46:56 -0700

    Update Cell.java

--- src/Cell.java
+++ src/Cell.java
@@ -1,1 +1,49 @@
 // placeholder
+public class Cell {
+  private String value;
+
+  public Cell() {
+    value = "";
+  }
+  
+  public Cell(String input) {
+    value = input;
+  }
+  
+  public String getValue() {
+    return value;
+  }
+  
+  public String toDisplay() {
+    
+    //truncate string to length - 1 and display >
+    int columnWidth = 12;
+    String output = value;
+    if (output.length() > columnWidth) {
+      output = output.substring(0, columnWidth - 1) + ">";
+    }
+    
+    //if length is odd pad end to even lenth
+    if(output.length() % 2 != 0) {
+      output += " ";
+    }
+    
+    //justify pad to center in cell
+    while (output.length() < columnWidth) {
+      output = " " + output + " ";
+    }
+    return output;
+  }
+  
+  public String toString() {
+    String result = getValue();
+    if (result.length() == 0) {
+      result = "<empty>";
+    }
+    return result;
+  }
+  
+  public void setValue (String input) {
+    value = input;
+  }
+}
